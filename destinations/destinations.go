73ecda1919a6b87c5c1c2eb95114ddd5ffc36e18
simon.wolfsteiner <simon.wolfsteiner@fiskaly.com>
2021-12-23 16:27:31 +0100

    added NewRemote function that returns a UDP connection

---
+++ destinations/destinations.go
@@ -0,0 +1,34 @@
+package destinations
+
+import (
+	"io"
+	"net"
+	"os"
+)
+
+var (
+	connection io.WriteCloser
+)
+
+func NewRemote() (io.Writer, error) {
+	// read remote logging destintion from environment
+	destination := os.Getenv("FISKALY_REMOTE_LOGGING_DESTINATION")
+	if destination == "" {
+		panic("env var FISKALY_REMOTE_LOGGING_DESTINATION not defined.")
+	}
+
+	// open UDP socket
+	if connection == nil {
+		resolved, err := net.ResolveUDPAddr("udp", destination)
+		if err != nil {
+			return nil, err
+		}
+
+		connection, err = net.DialUDP("udp", nil, resolved)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return connection, nil
+}
